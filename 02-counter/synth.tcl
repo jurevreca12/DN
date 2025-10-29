4a614a4a4bfb8a21063fa0b846aadc40967bf995
jvreca <jure.vreca@ijs.si>
2025-10-29 10:23:45 +0100

    Dodal vaja 2.

--- 02-counter/synth.tcl
+++ 02-counter/synth.tcl
@@ -4,23 +4,20 @@
 #
 #set_part xc7a750tcsg324-1
 set_part xc7a100tcsg324-1
-read_verilog gray_counter.sv
+read_verilog gray_counter2.sv
 read_xdc     design.xdc
 
 # Run synthesis
 synth_design -top gray_counter
-write_checkpoint -force ./post_synth
-report_timing_summary 
-report_power -file      post_synth_power.rpt
-report_clock_interaction	-file post_synth_clock_interaction.rpt -delay_type min_max
-report_high_fanout_nets		-file post_synth_high_fanout_nets.rpt  \
-												  -fanout_greater_than 200 \
-												  -max_nets 5
-write_verilog -force impl_netlist.v
-write_edif    -force impl_netlist.edif
+report_timing_summary    -file ./output/post_synth_timing_summary.rpt
+report_power             -file ./output/post_synth_power.rpt
+report_clock_interaction -file ./output/post_synth_clock_interaction.rpt -delay_type min_max
+report_high_fanout_nets	 -file ./output/post_synth_high_fanout_nets.rpt -fanout_greater_than 200 -max_nets 5
+write_verilog -force ./output/impl_netlist.v
+write_edif    -force ./output/impl_netlist.edif
 
 opt_design
 place_design
 route_design
 
-write_bitstream -force gray_counter.bit -bin_file
+write_bitstream -force ./output/gray_counter.bit -bin_file
